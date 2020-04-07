94c3b4fe128c7b0930791d506c14ae933015db33
FedericoNembrini <25205086+FedericoNembrini@users.noreply.github.com>
2020-04-07 18:11:22 +0200

    [master] Html, Css and Js rewrite Optimization.

--- scripts/sitescripts.js
+++ scripts/sitescripts.js
@@ -1,23 +1,25 @@
+var Songs =
+[
+    {
+        "name": "Garden Of Giants",
+        "artist": "Rici",
+        "album": "",
+        "url": "songs/GardenOfGiants.mp3",
+        "cover_art_url": "covers/No_Album.png"
+    },
+    {
+        "name": "Buio",
+        "artist": "Rici",
+        "album": "",
+        "url": "songs/Buio.mp3",
+        "cover_art_url": "covers/No_Album.png"
+    }
+];
+
 function InitializeWebSite(){
-    Amplitude.init({
-        "songs":
-        [
-            {
-                "name": "Garden Of Giants",
-                "artist": "Rici",
-                "album": "",
-                "url": "songs/GardenOfGiants.mp3",
-                "cover_art_url": "covers/No_Album.png"
-            },
-            {
-                "name": "Buio",
-                "artist": "Rici",
-                "album": "",
-                "url": "songs/Buio.mp3",
-                "cover_art_url": "covers/No_Album.png"
-            }
-        ]
-    });
+    AmplitudePlayersInit();
+    AmplitudeInit();
+    CloseLoadingHover();
     
     $('.amplitude-song-played-progress').click(function(e){
         var offset = this.getBoundingClientRect();
@@ -35,11 +37,30 @@ function InitializeWebSite(){
             });
         });
     });
-    
-    loadingCompleted();
 }
 
-function loadingCompleted(){
-    setTimeout(function() { $("#LoadingHover").fadeOut(800, function() {}); }, 200);
-    setTimeout(function() { $("#Content").fadeIn(800, function() {}); }, 200);
+function AmplitudePlayersInit() {
+    var songsNumber = Songs.length;
+    var BeatsCardBody = document.getElementById("BeatsCardBody");
+    var templateElement = document.getElementById("PlayerTemplate");
+
+    for(var i = 0; i < songsNumber; i++){
+        templateClone = templateElement.content.cloneNode(true);
+        templateClone.querySelectorAll('[data-amplitude-song-index=""]').forEach(function(element) {
+            $(element).attr("data-amplitude-song-index", this.valueOf());
+        }, i)
+        
+        BeatsCardBody.append(templateClone);
+    }
+}
+
+function AmplitudeInit() {
+    Amplitude.init({
+        "songs": Songs
+    });
+}
+
+function CloseLoadingHover(){
+    setTimeout(function() { $("#LoadingHover").fadeOut(800, function() {}); }, 400);
+    setTimeout(function() { $("#Content").fadeIn(800, function() { console.timeEnd("LoadingTime"); }); }, 400);
 }
